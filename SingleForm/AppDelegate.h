d5010996dfc93c68c2cc37af7813e7da7cc76e6c
Oleg Lobachev <aironik@gmail.com>
2017-08-02 10:35:26 +0300

    Initial Commit

---
+++ SingleForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleForm
+//
+//  Created by aironik on 02/08/2017.
+//  Copyright © 2017 aironik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleForm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleForm
+//
+//  Created by aironik on 02/08/2017.
+//  Copyright © 2017 aironik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
